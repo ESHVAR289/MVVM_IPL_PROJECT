b29deed9641cdf6faf94dc32e97fccfd1b9a924d
ESHVAR289 <eshvar89@hotmail.com>
2016-06-05 22:44:40 +0530

    Added Constant class which is required to define some constants required into the project.

---
+++ app/src/main/java/einfoplanet/com/ipl/constants/Constants.java
@@ -0,0 +1,27 @@
+package einfoplanet.com.ipl.constants;
+
+/**
+ * Class declared to represent all the constants which are required in project.
+ */
+
+public class Constants {
+    public static final String TEAM_NAME = "team_name";
+    public static final String IPL_BASE_URL = "http://192.168.0.121/ipl2016/";
+    public static final String TEAM_VIEW_URL = "team_info_with_folder.json";
+
+    public static final String KEY_TEAM_NAME = "team_name";
+    public static final String KEY_TEAM_IMG_URL = "team_img_url";
+    public static final String KEY_TEAM_CAPTAIN = "team_captain";
+    public static final String KEY_TEAM_COACH = "team_coach";
+    public static final String KEY_TEAM_HOME_VENUE = "team_home_venue";
+    public static final String KEY_TEAM_OWNER = "team_owner";
+
+    public static final String KEY_PLAYER_NAME = "player_name";
+    public static final String KEY_PLAYER_ROLE = "player_role";
+    public static final String KEY_PLAYER_BATTING_STYLE = "player_batting_style";
+    public static final String KEY_PLAYER_BOWLING_STYLE = "player_bowling_style";
+    public static final String KEY_PLAYER_DOB = "player_dob";
+    public static final String KEY_PLAYER_IMG_URL = "player_img_url";
+    public static final String KEY_PLAYER_NATIONALITY = "player_nationality";
+    public static final String FOLDER_NAME ="destination_folder_name";
+}
