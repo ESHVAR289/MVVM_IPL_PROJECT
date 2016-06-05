fe5619bee90166567948cd3ae48b776275fefb9a
ESHVAR289 <eshvar89@hotmail.com>
2016-06-05 20:52:53 +0530

    Added the RecyclerViewClickListener interface to handle the click listener for the recyclerview.

---
+++ app/src/main/java/einfoplanet/com/ipl/recyclerinterface/RecyclerClickListener.java
@@ -0,0 +1,11 @@
+package einfoplanet.com.ipl.recyclerinterface;
+
+import android.view.View;
+
+/**
+ * Created by eshvar289 on 5/6/16.
+ */
+
+public interface RecyclerClickListener {
+    void onClick(View view, int position);
+}
